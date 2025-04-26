dfdbe8a7919d1ade7378f0d296a12ea1ba4eb21e
Mirian Quispe <mirianquispe1505@gmail.com>
2025-04-26 20:54:16 -0300

    Add unit test for MiniPicpay application context loading

---
+++ src/test/java/com/quispe/mini_picpay/MiniPicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quispe.mini_picpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniPicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
