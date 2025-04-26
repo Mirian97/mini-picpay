4b58404ce1304d4b3865204621dd7a269aa5517a
Mirian Quispe <mirianquispe1505@gmail.com>
2025-04-26 20:39:33 -0300

    Add MiniPicpayApplication class with Spring Boot configuration

---
+++ java/com/quispe/mini_picpay/MiniPicpayApplication.java
@@ -0,0 +1,15 @@
+package com.quispe.mini_picpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing;
+
+@EnableJdbcAuditing
+@SpringBootApplication
+public class MiniPicpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniPicpayApplication.class, args);
+	}
+
+}
